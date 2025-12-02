0465adec367c8c4807f820090e7c181b2fa14279
hanifYusoff <hanifUMK@gmail.com>
2025-12-02 14:01:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c36s2cckgeje.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "hanifyusoff-employee"
+customregion = "us-east-1"
+
